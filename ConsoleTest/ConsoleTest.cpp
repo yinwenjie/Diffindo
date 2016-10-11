f91416b85897dce224115484d4fc03fe2dc06220
yinwenjie <eternal.ywj@gmail.com>
2016-10-11 19:13:01 +0800

    打印时间轴

--- ConsoleTest/ConsoleTest.cpp
+++ ConsoleTest/ConsoleTest.cpp
@@ -6,8 +6,29 @@
 #include <iostream>
 using namespace std;
 
+ofstream timeline;
+int videoIdx = 0, audioIdx = 0;
+
 void edit_tag(CFlvTag *tag)
 {
+	timeline << "-----------------------------" << endl;
+	if (tag->Get_tag_type() == TAG_TYPE_VIDEO)
+	{
+		timeline << "Video Idx: " << videoIdx++ << endl;
+		timeline << "Time Stamp: " << to_string(tag->Get_tag_timestamp_ext())<< endl;
+	}
+	else if (tag->Get_tag_type() == TAG_TYPE_AUDIO)
+	{
+		if (audioIdx < 1)
+		{
+			audioIdx++;
+			return;
+		}
+		double actualTime = (audioIdx - 1) * 1024000.0 / 22050.0;
+		timeline << "Audio Idx: " << audioIdx++ << endl;
+		timeline << "Time Stamp: " << to_string(tag->Get_tag_timestamp_ext()) << endl;
+		timeline << "Actual Time: " << to_string(actualTime) << endl;
+	}
 	return;
 }
 
@@ -22,7 +43,8 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	return 0;
+	timeline.open("timeline.txt");
+	timeline << argv[1] << endl;
 
 	CFlvWriter writer(argv[2], &parser);
 	if (writer.Init(true, true))
@@ -30,9 +52,11 @@ int main(int argc, char **argv)
 		cout << argv[2] << " : " << errorHints[-err] << endl;
 		return -1;
 	}
-	writer.Clone_FLV_with_video();
-//	writer.Create_FLV_with_edited_tag(edit_tag);
+//	writer.Clone_FLV_with_video();
+	writer.Create_FLV_with_edited_tag(edit_tag);
 //	writer.Extract_tags_with_range(0, 10000);
+
+	timeline.close();
 
     return 0;
 }
