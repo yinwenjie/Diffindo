a817baa5b0e425e5ca5f886cca7c9ea69e5a6260
yinwenjie <eternal.ywj@gmail.com>
2016-12-22 19:07:10 +0800

    添加MP4ParserLib工程

---
+++ MP4ParserLib/src/DataTypes.h
@@ -0,0 +1,11 @@
+#ifndef _DATA_TYPES_H_
+#define _DATA_TYPES_H_
+
+// Data type...
+typedef unsigned char	UINT8;
+typedef unsigned short	UINT16;
+typedef unsigned int	UINT32;
+typedef unsigned long long UINT64;
+typedef UINT8 BYTE;
+
+#endif
